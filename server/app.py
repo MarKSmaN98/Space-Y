265c115651d44b729c530b08a77ad5bea12b040c
Kevin Salinas <kevinsalinas155@gmail.com>
2023-04-17 14:51:40 -0400

    added astronauts route backend

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
 
 # Local imports
@@ -11,6 +11,25 @@ from config import app, db, api
 from models import Spaceship, Astronaut, Mission
 
 # Views go here!
+
+class Astronauts (Resource):
+
+    def get(self):
+        
+        a_list = []
+        for a in Astronaut.query.all():
+            a_dict = {
+                'name': a.name,
+                'age': a.age,
+                'weight': a.weight
+            }
+            a_list.append(a_dict)
+
+        response = make_response(a_list, 200)
+
+        return response
+
+api.add_resource(Astronauts, '/astronauts')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
