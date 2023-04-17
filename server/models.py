befaa66fd85e79914946c3e95683a1c8ea257ea9
Mark Coats <alexco1234@gmail.com>
2023-04-17 10:17:27 -0500

    ramping up

--- server/models.py
+++ server/models.py
@@ -8,10 +8,19 @@ from config import db
 # Models go here!
 
 class Astronaut(db.Model, SerializerMixin):
+    __tablename__ = 'astronauts'
+
+    id = db.Column(db.Integer, primary_key=True)
     pass
 
 class Spaceship(db.Model, SerializerMixin):
+    __tablename__ = 'spaceships'
+
+    id = db.Column(db.Integer, primary_key=True)
     pass
 
 class Mission(db.Model, SerializerMixin):
+    __tablename__ = 'missions'
+
+    id = db.Column(db.Integer, primary_key=True)
     pass
